db3b7539edc30c39e7c9f0c882ed92a583dbeca0
xLaura3m5 <laura.haenel@3m5.de>
2024-04-20 14:06:29 +0200

    [wip] tacotruck movement depending on tacocat

--- Scenes/Tacocat.gd
+++ Scenes/Tacocat.gd
@@ -5,11 +5,18 @@ var inputs = {"right": Vector2.RIGHT,
 			"left": Vector2.LEFT,
 			"up": Vector2.UP,
 			"down": Vector2.DOWN}
+			
+@export var sprite: Sprite2D
+@export var parentNode: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
+	tile_size = sprite.texture.get_size() / 2
+	print(tile_size)
+	parentNode.position = position.snapped(Vector2.ONE * tile_size)
+	parentNode.position += Vector2.ONE * tile_size/2
 
 func _unhandled_input(event):
 	for dir in inputs.keys():
@@ -18,3 +25,4 @@ func _unhandled_input(event):
 
 func move(dir):
 	position += inputs[dir] * tile_size
+	parentNode.position += inputs[dir] * tile_size
