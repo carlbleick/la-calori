da65b780e1b6b965d37a8fe19edc2c838e609d76
xLaura3m5 <laura.haenel@3m5.de>
2024-04-20 14:35:01 +0200

    move parentnode

--- Scenes/Tacocat.gd
+++ Scenes/Tacocat.gd
@@ -1,19 +1,19 @@
 extends Area2D
 
-var tile_size = 64
+var tile_size = 120
 var inputs = {"right": Vector2.RIGHT,
 			"left": Vector2.LEFT,
 			"up": Vector2.UP,
 			"down": Vector2.DOWN}
-			
+@onready var ray = $RayCast2D
 @export var sprite: Sprite2D
 @export var parentNode: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
-	tile_size = sprite.texture.get_size() / 2
+	#tile_size = sprite.texture.get_size() / 2
+	#position = position.snapped(Vector2.ONE * tile_size)
+	#position += Vector2.ONE * tile_size/2
 	print(tile_size)
 	parentNode.position = position.snapped(Vector2.ONE * tile_size)
 	parentNode.position += Vector2.ONE * tile_size/2
@@ -24,5 +24,9 @@ func _unhandled_input(event):
 			move(dir)
 
 func move(dir):
-	position += inputs[dir] * tile_size
-	parentNode.position += inputs[dir] * tile_size
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += inputs[dir] * tile_size / 3
+		parentNode.position += inputs[dir] * tile_size
+	
