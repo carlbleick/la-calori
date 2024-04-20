ac8c7e612cf40208406cc07da25acf53aded26ba
xLaura3m5 <laura.haenel@3m5.de>
2024-04-20 12:31:38 +0200

    Add Tacocat moving on grid

---
+++ Scenes/Tacocat.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tile_size
