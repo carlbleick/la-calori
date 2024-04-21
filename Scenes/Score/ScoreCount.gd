27b8629335790c32e552d9c53d7d44d63a9ddc13
xLaura3m5 <laura.haenel@3m5.de>
2024-04-21 18:22:37 +0200

    update animation

--- Scenes/Score/ScoreCount.gd
+++ Scenes/Score/ScoreCount.gd
@@ -2,9 +2,12 @@ extends Label
 
 var score = 0
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 func _ready():
 	Events.order_delivered.connect(_on_events_order_delivered)
 	_update_text()
+	_animated_sprite.play("default")
 	
 func _update_text():
 	text = "%04d" % score
