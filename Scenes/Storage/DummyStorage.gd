77770423a58a2b78a12199e27404812e5c077f70
Carl Bleick <frozencarldesigns@gmail.com>
2024-04-21 10:12:50 +0200

    add collected ingredient to storage amount

--- Scenes/Storage/DummyStorage.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _on_spaceship_dummy_ingredient_collected(ingredient_id, amount):
-	print("Ingredient {ingredient_id} collected: Amount {amount}!".format({"ingredient_id":ingredient_id, "amount":amount}))
-	pass # Replace with function body.
