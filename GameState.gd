db5d3670a8cc8f4afa0627b25d62f41538b2dbb8
Richard Böhme <richard.boehme1999@gmail.com>
2024-04-21 18:22:39 +0200

    order probabilities

--- GameState.gd
+++ GameState.gd
@@ -1,6 +1,18 @@
 extends Node
 
 var orders = []
+var gameTime = 0 
+var order_probability = 0.1
+var lastTime
+
+func _ready():
+	lastTime = Time.get_ticks_msec()
+
+func _process(_delta):
+	var newTime = Time.get_ticks_msec()
+	if newTime - lastTime >= 1000:
+		order_probability += 0.0005
+		lastTime = newTime
 
 func remove_order(instance_id):
 	for i in range(orders.size()):
--- Scenes/Ordering/OrderManager.gd
+++ Scenes/Ordering/OrderManager.gd
@@ -1,13 +1,29 @@
 extends Node
 
 @export var orderables: Array[OrderCard]
+@export var probabilities: Array[float]
 @export var max_orders = 10
 
 @onready var container = $HBoxContainer
 
+func _ready():
+	assert(probabilities.size() == orderables.size())
+
 func _on_timer_timeout():
+	if randf() >= GameState.order_probability:
+		return
+	
 	if GameState.orders.size() <= max_orders:
-		var orderable = orderables[randi() % orderables.size()].duplicate()	
+		var random_f = randf()
+		var index = 0
+		for i in range(probabilities.size()):
+			var prob = probabilities[i]
+			if random_f <= prob:
+				index = i
+				break
+			else:
+				random_f -= prob
+		var orderable = orderables[index].duplicate()	
 		GameState.orders.push_back(orderable)
 		container.add_child(orderable)
 		orderable.start()
