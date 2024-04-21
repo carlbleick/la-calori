225c2188a478311cefcca2e441da2cb8196982e2
xLaura3m5 <laura.haenel@3m5.de>
2024-04-21 11:33:50 +0200

    Added placing of sprites on counter

--- Scenes/KitchenAppliances/KitchenCounter.gd
+++ Scenes/KitchenAppliances/KitchenCounter.gd
@@ -9,6 +9,19 @@ var current_processing_time = 0.0
 
 @onready var progress_bar = $TextureProgressBar
 
+var food_textures = {
+	Constants.IngredientType.TACO: preload("res://Assets/Sprites/food-marker-taco.png"),
+	Constants.IngredientType.VEGGIES: preload("res://Assets/Sprites/food-marker-veggies.png"),
+	Constants.IngredientType.PROTEIN: preload("res://Assets/Sprites/food-marker-protein.png"),
+	#Constants.IngredientType.VEGGIES_CUTTED: preload("res://Assets/Sprites/food-veggies--cutted.png"),
+	#Constants.IngredientType.PROTEIN_COOKED: preload("res://Assets/Sprites/food-protein--cooked.png"),
+	#Constants.IngredientType.PROTEIN_OVERCOOKED: preload("res://Assets/Sprites/food-protein--overcooked.png"),
+	#Constants.IngredientType.TACO_VEGGIES: preload("res://Assets/Sprites/food-taco--veggies.png"),
+	#Constants.IngredientType.TACO_PROTEIN: preload("res://Assets/Sprites/food-taco--protein.png"),
+	#Constants.IngredientType.TACO_FULL: preload("res://Assets/Sprites/food-taco--full.png"),
+}
+var current_food_sprite: Sprite2D
+
 func _process(delta):
 	if current_processing_time > 0:
 		if Input.is_action_pressed("process"):
@@ -67,12 +80,21 @@ func _on_tacocat_request_place_item(instance_id, ingredient):
 					item_placed.emit()
 				else:
 					print("You cannot place " + Constants.IngredientType.keys()[ingredient] + " on " + Constants.IngredientType.keys()[current_item])
+		if food_textures.has(current_item):
+			var sprite_node = Sprite2D.new()
+			sprite_node.texture = food_textures[current_item]
+			sprite_node.z_index = 2
+			current_food_sprite = sprite_node
+			add_child(sprite_node)
 
 func _on_tacocat_request_take_item(instance_id):
 	if instance_id == get_instance_id() && current_item != Constants.IngredientType.NONE:
 		print("Item " + Constants.IngredientType.keys()[current_item] + " taken from counter")
 		item_taken.emit(current_item)
 		current_item = Constants.IngredientType.NONE
+		if current_food_sprite:
+			current_food_sprite.queue_free()
+			current_food_sprite = null
 
 
 func _on_tacocat_request_process_item(instance_id):
